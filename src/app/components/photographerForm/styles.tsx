d5ba36863daaa33b03e4248b8a0c1d5a5c5291ea
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2023-02-26 17:25:54 -0300

    refactor(register-form): adapting to be dynamic

---
+++ src/app/components/photographerForm/styles.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+export const Form = styled.form``;
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow-x: hidden;
+`;
+
+export const ContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 400px;
+    padding: 30px;
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.colors.primary.dark};
+
+    button {
+        margin-top: 30px;
+        width: 100%;
+    }
+`;
+
+export const FormRow = styled.div`
+  margin-top: 16px;
+  width: 100%;
+`;
+
+export const TextLogin = styled.span`
+    font-size: 36px;
+    font-weight: 600;
+    color: ${(({ theme }) => theme.colors.primary.lighter)};
+    margin-bottom: 8px;
+`;
+
+export const TextWelcome = styled.span`
+    font-size: 14px;
+    color: ${(({ theme }) => theme.colors.primary.lighter)};
+    margin-left: 16px;
+`;
+
+export const FormGroup = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+
+    div + div {
+        margin-left: 16px;
+        width: 40%;
+    }
+`;
+
+export const TextRegister = styled.span`
+    font-size: 12px;
+    color: ${(({ theme }) => theme.colors.primary.lighter)};
+    margin-top: 16px;
+
+    strong {
+        text-decoration: underline;
+    }
+`;
+
+export const ErrorMessage = styled.span`
+    color: ${(({ theme }) => theme.colors.danger.light)};
+    font-size: 10px;
+`;
