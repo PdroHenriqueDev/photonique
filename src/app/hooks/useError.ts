5cecc2746ff13b16c97224f68dc48c80e3bfabe7
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2023-02-25 23:51:19 -0300

    feat: adding error hook

---
+++ src/app/hooks/useError.ts
@@ -0,0 +1,33 @@
+import { UseErrorProps } from "app/models/hooks/useError.mode";
+import { useState } from "react";
+
+export const UseError = (): any => {
+    const [errors, setErrors] = useState<UseErrorProps[]>([]);
+
+    const setError = ({ field, message }: UseErrorProps) => {
+        const errorAlreadyExists = errors.find(
+            (error) => error.field === field
+        );
+        if (errorAlreadyExists) {
+            return;
+        }
+
+        setErrors((prevState) => [...prevState, { field, message }]);
+    };
+
+    const removeError = (fieldName: string): void => {
+        setErrors((prevState) =>
+            prevState.filter(({ field }) => field !== fieldName)
+        );
+    }
+
+    const getErrorMessageByFieldName = (fieldName: string) =>
+        errors.find(({ field }) => field === fieldName)?.message;
+
+    return {
+        errors,
+        setError,
+        removeError,
+        getErrorMessageByFieldName
+    };
+}
