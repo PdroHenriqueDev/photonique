d7c3594c86e8013924adbfddf9e8e578a308ff05
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2023-03-01 18:23:59 -0300

    feat: adding .env file

--- src/app/pages/register/index.tsx
+++ src/app/pages/register/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from './styles';
-import PhotographerForm from '../../components/photographerForm'
+import PhotographerForm from '../../components/photographerForm';
+import PhotographerService from '../../services/PhotographerService';
 
 function Register() {
 
  async function handleSubmit(formData: any) {
     console.log(formData);
+    console.log(PhotographerService.createPhotographer(formData));
  }
 
   return (
