7e65016941b0faf35a067c1180441700955f375b
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2023-03-25 23:58:11 -0300

    refactor(indetify): deleting

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginComponent from './pages/login';
+import HomeComponent from './pages/home';
 import { lazy, Suspense } from 'react';
 
 const RegisterForm = lazy(() => import('./pages/register'));
@@ -7,7 +8,8 @@ const RegisterForm = lazy(() => import('./pages/register'));
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/*" element={<LoginComponent />} />
+      <Route path="/" element={<HomeComponent />} />
+      <Route path="/login/*" element={<LoginComponent />} />
       <Route
         path="/cadastrar"
         element={
