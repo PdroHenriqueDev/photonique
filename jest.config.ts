c2cf4ea56faa245a4b9b7c6471b75df3233b5050
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2023-04-06 20:50:38 -0300

    test: adding coverage command

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,10 @@
-export default {
+import type { Config } from 'jest';
+
+const config: Config = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.ts'],
+  coverageDirectory: './coverage',
+  testMatch: ['<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)'],
 };
+
+export default config;
